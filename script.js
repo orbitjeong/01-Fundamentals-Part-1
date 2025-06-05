aeb2d6a3f58f286e72397a1d186b124d08facf52
orbitjeong <jhj9708@hotmail.com>
2025-06-05 09:27:22 -0400

    9~10

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ let firstName = "Jonas";
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+
+let country = "South Korea";
+let continent = "Asia";
+let population = 51.7;
+
+// assignment
+// Values and Variables
+// 1. Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+// 2. Log their values to the console.
+
+console.log(country);
+console.log(continent);
+console.log(population);
